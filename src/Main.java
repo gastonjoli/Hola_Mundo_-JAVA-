16ac8304e923841eb8b291d85251dd698467c0c7
Gaston Jolinont <falsee63@gmail.com>
2021-09-28 11:49:05 -0300

    Practicando subir desde Intellij

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Master");
+
+        var  holayminombre ="Hola Me llamo Gaston";
+
+        var edad  = 34;
+
+        String  favorito = "Me gusta escribir codigo en python y java";
+
+        System.out.println( holayminombre + " " + "y tengo: "+ edad +" "+ "años de edad");
+
+        System.out.println(favorito);
+
+    }
+}
