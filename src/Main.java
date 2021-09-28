cdd406442cbf23248d9a0ed04080310efe0ba3fe
Gaston Jolinont <falsee63@gmail.com>
2021-09-28 12:22:46 -0300

    Actualizando archivos en GITHUB desde Intellij

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hola Master");
+        System.out.println("Hola Gente");
 
-        var  holayminombre ="Hola Me llamo Gaston";
+        var  holayminombre =" Me llamo Gaston Jolinont ";
 
-        var edad  = 34;
+        var edad  = 34 ;
 
         String  favorito = "Me gusta escribir codigo en python y java";
 
         String practica = "Estoy practicando como subir a GITHUB desde intellig";
 
+        var saludos = "saludos";
+
         System.out.println( holayminombre + " " + "y tengo: "+ edad +" "+ "años de edad");
 
         System.out.println(favorito);
         System.out.println(practica);
+        System.out.println(saludos);
 
     }
 }
